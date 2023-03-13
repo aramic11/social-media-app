ef988b09b128d1de7db148c239efc7219b4ce331
Ahmed Ramic <ahmedramic@Ahmeds-MacBook-Air.local>
2023-03-13 13:56:54 -0400

    imported graphql from appollo server and made different files for the user, comment, follow, like, and post

---
+++ server/schema/Comment.js
@@ -0,0 +1,22 @@
+import { gql } from "apollo-server-express";
+
+// Define Comment schema
+const CommentSchema = gql`
+  type Comment {
+    id: ID!
+    username: String!
+    content: String!
+    dateCreated: String!
+    commentLikes: [CommentLike]!
+    commentLikeCount: Int!
+  }
+
+  extend type Mutation {
+    createComment(postId: String!, content: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    updateComment(postId: ID!, commentId: ID!, content: String!): Post!
+  }
+`;
+
+export default CommentSchema;
+
