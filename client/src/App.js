3bdd37b98ec547e4dd3a30d3c04fa56ed93c7938
unknown <sylvynhaarc@hotmail.com>
2023-03-14 16:08:56 -0500

    app.js file updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Container } from "@material-ui/core";
-import { StoreProvider } from "./store/store";
+//import { BrowserRouter as Router, Route } from "react-router-dom";
+//import { Container } from "@material-ui/core";
+//import { StoreProvider } from "./store/store";
 import "./App.css";
+import Chat from './Chat'
 
 const App = () => {
   return (
+    <Chat/>
     // Wrap the entire app with StoreProvider to make the store available to all components
-    <StoreProvider>
-      <Router>
-        <Container>
-          {/* The Navbar component is displayed on every page */}
-          <Navbar />
+    // <StoreProvider>
+    //   <Chat/>
+    //   <Router>
+    //     <Container>
+    //       {/* The Navbar component is displayed on every page */}
+    //       <Navbar />
           
-          {/* Define routes for each page */}
+    //       {/* Define routes for each page */}
          
-        </Container>
-      </Router>
-    </StoreProvider>
+    //     </Container>
+    //   </Router>
+    // </StoreProvider>
   );
 };
 
